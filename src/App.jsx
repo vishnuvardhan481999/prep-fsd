4de51db19f3a4553f065ce6b5cced80b248385ba
vishnuvardhan481999 <vishnuvardhan481999@gmail.com>
2024-04-05 09:29:52 +0530

    basic FSD

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Greet/>
+   </>
+
+  );
+}
+function Greet() {
+  return (
+    <div>
+      <h1>Welcome to react</h1>
+    </div>
+  );
+}
+
+
+export default App
